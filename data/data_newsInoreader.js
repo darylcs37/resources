37e362d9175d84bf8ba8205b57f716181680abeb
Dario <64671555+darylcs37@users.noreply.github.com>
2022-11-13 19:03:22 +0800

    Update data_newsInoreader.js

--- data/data_newsInoreader.js
+++ data/data_newsInoreader.js
@@ -1,47 +1,44 @@
 	var aDataSet = [
 		//--------------------------------   START   --------------------------------
 		// News Inoreader - aggregated news
-		// ['News - XXXX','Aggregated News Feeds (inoreader)','XXXX','News-Aggregated','XXXXX'],
-		// ['News - XXXX','Aggregated News Feeds (inoreader)','XXXX','News-Aggregated','XXXXX'],
-		// ['News - XXXX','Aggregated News Feeds (inoreader)','XXXX','News-Aggregated','XXXXX'],
+		// ['News - XXXX','Aggregated News Feeds (inoreader)','XXXX','News-Aggregated','XXXXX','XXXXX'],
 		//
 		//
 		//
 		//
 
-		['News - 5G','Aggregated News Feeds (inoreader)','5G','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/5G%20Telco/view/html'],
-		['News - Air /Aviation','Aggregated News Feeds (inoreader)','Air / Aviation','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20Aviation/view/html'],
-		['News - AR/VR/Mixed Reality/Metaverse','Aggregated News Feeds (inoreader)','AR-VR','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/XR%20AR%20MR/view/html,https://www.inoreader.com/stream/user/1004633940/tag/XR/view/html'],
-		['News - Armament','Aggregated News Feeds (inoreader)','Armament','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Armament/view/html'],
-		['News - Cyber-Info','Aggregated News Feeds (inoreader)','Cyber-Info','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Cyber-Info/view/html'],
-		['News - Data Analytics','Aggregated News Feeds (inoreader)','Data Analytics','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Data%20Analytics/view/html'],
-		['News - EW / Radar / ELINT','Aggregated News Feeds (inoreader)','EW','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/EW%20Radar%20ELINT/view/html'],
-		['News - GIS 3D Mapping','Aggregated News Feeds (inoreader)','GIS 3D Mapping','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/GIS%203D%20Mapping/view/html'],
-		['News - General','Aggregated News Feeds (inoreader)','General','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20General/view/html'],
-		['News - Greyzone Hybrid Warfare','Aggregated News Feeds (inoreader)','Greyzone Hybrid Warfare','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Greyzone%20%2F%20Hybrid%20Warfare/view/html'],
-		['News - Hypersonic','Aggregated News Feeds (inoreader)','Hypersonic','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Hypersonic/view/html'],
-		['News - Infrastructure','Aggregated News Feeds (inoreader)','Infrastructure','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Infrastructure/view/html'],
-		['News - IoT','Aggregated News Feeds (inoreader)','IoT','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/IoT/view/html'],
-		['News - Learning & Development','Aggregated News Feeds (inoreader)','Learning & Development','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Learning%20%26%20Development/view/html'],
-		['News - MY','Aggregated News Feeds (inoreader)','MY','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20MY/view/html'],
-		['News - SG','Aggregated News Feeds (inoreader)','SG','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20SG/view/html'],
-		['News - CN','Aggregated News Feeds (inoreader)','CN','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20China/view/html'],
-		['News - Research Labs (Industry / Academia / Govt)','Aggregated News Feeds (inoreader)','Research Labs','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Research%20Labs%20%28Industry%20%2F%20Academia%20%2F%20Govt%29/view/html'],
-		['News - Robotics (General)','Aggregated News Feeds (inoreader)','Drone','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Robot%20-%20General/view/html'],
-		['News - Robotics (Land)','Aggregated News Feeds (inoreader)','Drone','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Robot%20-%20Land/view/html'],
-		['News - Russia Ukraine','Aggregated News Feeds (inoreader)','RUS-UKR','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20RUS-UKR/view/html'],
-		['News - Russia Ukraine (tagged)','Aggregated News Feeds (inoreader)','RUS-UKR','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/RUS-UKR/view/html'],
-		['News - Sea / Maritime','Aggregated News Feeds (inoreader)','Sea / Maritime','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20Maritime/view/html'],
-		['News - Semi-Conductor','Aggregated News Feeds (inoreader)','Semi-Conductor','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/SemiConductor/view/html'],
-		['News - Social Media MarTech','Aggregated News Feeds (inoreader)','Social Media MarTech','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Social%20Media%20MarTech/view/html?c=0x122E4A&bc=0xF8F2F2'],
-		['News - Software Engineering','Aggregated News Feeds (inoreader)','Software Engineering','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Software%20Engineering/view/html'],
-		['News - Space','Aggregated News Feeds (inoreader)','Space','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Space/view/html'],
-		['News - Training & Simulation','Aggregated News Feeds (inoreader)','Training & Simulation','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Training%20%26%20Simulation/view/html'],
-		['News - UI/UX','Aggregated News Feeds (inoreader)','UI/UX','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/UI%20UX%20HCI/view/html'],
-		['Resources - Russia Ukraine (curated)','Curated list of RUS-UKR resources','RUS-UKR','News-Aggregated','https://darylcs37.github.io/resources/listing_resources_rus-ukr.htm'],
-		['Resources - China (curated)','Curated list of China resources','China','News-Aggregated','https://darylcs37.github.io/resources/listing_resources_cn.htm'],
-		['Resource - BH 2023 SeeTrue Workshop','Aggregated News Feeds (inoreader)','SeeTrue','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/D4%20BrainHack/view/html'],
-		['Videos - Channel News Asia (Youtube)','Aggregated News Feeds (inoreader)','Videos','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/YouTube/view/html'],
+		['News - 5G','Aggregated News Feeds (inoreader)','5G','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/5G%20Telco/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/5G%20Telco'],
+		['News - Air /Aviation','Aggregated News Feeds (inoreader)','Air / Aviation','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20Aviation/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20Aviation'],
+		['News - AR/VR/Mixed Reality/Metaverse','Aggregated News Feeds (inoreader)','AR-VR','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/XR%20AR%20MR/view/html,https://www.inoreader.com/stream/user/1004633940/tag/XR/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/XR'],
+		['News - Armament','Aggregated News Feeds (inoreader)','Armament','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Armament/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Armament'],
+		['News - Cyber-Info','Aggregated News Feeds (inoreader)','Cyber-Info','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Cyber-Info/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Cyber-Info'],
+		['News - EW / Radar / ELINT','Aggregated News Feeds (inoreader)','EW','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/EW%20Radar%20ELINT/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/EW%20Radar%20ELINT'],
+		['News - GIS 3D Mapping','Aggregated News Feeds (inoreader)','GIS 3D Mapping','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/GIS%203D%20Mapping/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/GIS%203D%20Mapping'],
+		['News - General','Aggregated News Feeds (inoreader)','General','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20General/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20General'],
+		['News - Greyzone Hybrid Warfare','Aggregated News Feeds (inoreader)','Greyzone Hybrid Warfare','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Greyzone%20%2F%20Hybrid%20Warfare/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Greyzone%20%2F%20Hybrid%20Warfare'],
+		['News - Hypersonic','Aggregated News Feeds (inoreader)','Hypersonic','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Hypersonic/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Hypersonic'],
+		['News - Infrastructure','Aggregated News Feeds (inoreader)','Infrastructure','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Infrastructure/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Infrastructure'],
+		['News - IoT','Aggregated News Feeds (inoreader)','IoT','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/IoT/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/IoT'],
+		['News - Learning & Development','Aggregated News Feeds (inoreader)','Learning & Development','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Learning%20%26%20Development/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Learning%20%26%20Development'],
+		['News - CN','Aggregated News Feeds (inoreader)','CN','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20China/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20China'],
+		['News - MY','Aggregated News Feeds (inoreader)','MY','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20MY/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20MY'],
+		['News - SG','Aggregated News Feeds (inoreader)','SG','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20SG/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20SG'],
+		['News - Research Labs (Industry / Academia / Govt)','Aggregated News Feeds (inoreader)','Research Labs','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Research%20Labs%20%28Industry%20%2F%20Academia%20%2F%20Govt%29/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Research%20Labs%20%28Industry%20%2F%20Academia%20%2F%20Govt%29'],
+		['News - Robotics (General)','Aggregated News Feeds (inoreader)','Drone','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Robot%20-%20General/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Robot%20-%20General'],
+		['News - Robotics (Land)','Aggregated News Feeds (inoreader)','Ground Robots','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Robot%20-%20Land/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Robot%20-%20Land'],
+		['News - Russia Ukraine','Aggregated News Feeds (inoreader)','RUS-UKR','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20RUS-UKR/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20RUS-UKR'],
+		['News - Russia Ukraine (tagged)','Aggregated News Feeds (inoreader)','RUS-UKR','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/RUS-UKR/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/RUS-UKR'],
+		['News - Sea / Maritime','Aggregated News Feeds (inoreader)','Sea / Maritime','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20Maritime/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/News%20-%20Maritime'],
+		['News - Semi-Conductor','Aggregated News Feeds (inoreader)','Semi-Conductor','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/SemiConductor/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/SemiConductor'],
+		['News - Social Media MarTech','Aggregated News Feeds (inoreader)','Social Media MarTech','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Social%20Media%20MarTech/view/html?c=0x122E4A&bc=0xF8F2F2', 'https://www.inoreader.com/stream/user/1004633940/tag/Social%20Media%20MarTech'],
+		['News - Software Engineering','Aggregated News Feeds (inoreader)','Software Engineering','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Software%20Engineering/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Software%20Engineering'],
+		['News - Space','Aggregated News Feeds (inoreader)','Space','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Space/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Space'],
+		['News - Training & Simulation','Aggregated News Feeds (inoreader)','Training & Simulation','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/Training%20%26%20Simulation/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/Training%20%26%20Simulation'],
+		['News - UI/UX','Aggregated News Feeds (inoreader)','UI/UX','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/UI%20UX%20HCI/view/html', 'XXXXX'],
+		['Resources - Russia Ukraine (curated)','Curated list of RUS-UKR resources','RUS-UKR','News-Aggregated','https://darylcs37.github.io/resources/listing_resources_rus-ukr.htm', 'https://darylcs37.github.io/resources/listing_resources_rus-ukr.htm'],
+		['Resources - China (curated)','Curated list of China resources','China','News-Aggregated','https://darylcs37.github.io/resources/listing_resources_cn.htm', 'https://darylcs37.github.io/resources/listing_resources_cn.htm'],
+		['Resource - BH 2023 SeeTrue Workshop','Aggregated News Feeds (inoreader)','SeeTrue','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/D4%20BrainHack/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/D4%20BrainHack'],
+		['Videos - Channel News Asia (Youtube)','Aggregated News Feeds (inoreader)','Videos','News-Aggregated','https://www.inoreader.com/stream/user/1004633940/tag/YouTube/view/html', 'https://www.inoreader.com/stream/user/1004633940/tag/YouTube'],
 
 		//--------------------------------   END   --------------------------------
 	];
@@ -56,7 +53,7 @@ $(document).ready(function() {	$('#dynamic').html( '<table cellpadding="0" style
 		"oSearch": {"sSearch": ""},
 		"aoColumns": [
 			{ "sTitle": "Domain Area", "sWidth": "35%" },
-			{ "sTitle": "Description", "sWidth": "40%" },
+			{ "sTitle": "Description", "sWidth": "35%" },
 			{ "sTitle": "Domain", "sWidth": "10%" }, // AI, Cyber, Space, Defence & Aerospace, 5G, Cloud, Autonomy. Robotics
 			{ "sTitle": "Category", "sWidth": "10%" }, // Algorithms, Policy, Patent, Commercial, Government
 			{ "sTitle": "URL", "sWidth": "5%",
@@ -88,6 +85,38 @@ $(document).ready(function() {	$('#dynamic').html( '<table cellpadding="0" style
 
 					// display Hyperline
 					// return data + ', <a href="' + full[5] + '" target="_blank">Link</a>';
+					return sResult;
+        }
+		  },
+			{ "sTitle": "RSS", "sWidth": "5%",
+			"mRender": function ( data, type, full ) {
+		 			// 'full' is the row's data object, and 'data' is this column's data
+		 			// e.g. 'full[0]' is the first item, and 'data' is the URL
+
+					// display Hyperline
+		 			// return '<a href="' + full[6] + '" target="_blank">' + data + '</a>';
+					var arrURL = data.split(',');
+					// console.log('no. of tokens='+ arrURL.length +' for ['+ data +']');
+					var sResult = '';
+					if (arrURL.length>1) {
+						for(var i=0; i<arrURL.length; i++) {
+							// display URL and Link
+							// sResult = sResult + arrURL[i] + ', <a href="' + arrURL[i] + '" target="_blank">Link</a>, ';
+
+							// display Link ONLY
+							sResult = sResult + '<a href="' + arrURL[i] + '" target="_blank">Link</a>, ';
+						}
+					}
+					else {
+						// display URL and Link
+						// sResult =  data + ', <a href="' + full[5] + '" target="_blank">Link</a>';
+
+						// display Link ONLY
+						sResult =  '<a href="' + full[5] + '" target="_blank">Link</a>';
+					}
+
+					// display Hyperline
+					// return data + ', <a href="' + full[6] + '" target="_blank">Link</a>';
 					return sResult;
         }
 		  },
